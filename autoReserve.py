991c128c7211084680f9ee2fc5146ae117b161f8
Kubrick <627773793@qq.com>
2022-01-21 01:23:39 +0800

    增加自动预约的时间输出，可以重定向到日志里

--- autoReserve.py
+++ autoReserve.py
@@ -1,7 +1,9 @@
 from libReserve import make_lib_resv
 import pandas as pd
+import time
 
 if __name__ == '__main__':
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     dataframe = pd.read_csv('info.csv', converters={u'stid': str, u'pwd': str, u'lib': str})
     for _, row in dataframe.iterrows():
         make_lib_resv(int(row['stid']), (row['pwd']), (row['lib']))
